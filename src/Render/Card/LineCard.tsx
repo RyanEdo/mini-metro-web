e03025325eb0d2b4ff0dad2347878d08499df23c
ryan <ryanedo@qq.com>
2024-03-18 15:51:58 +0800

    edit panel

--- src/Render/Card/LineCard.tsx
+++ src/Render/Card/LineCard.tsx
@@ -6,6 +6,7 @@ import "./LineCard.scss";
 import arrowIcon from "../../Resource/Icon/arrow.right.circle.fill.svg";
 import expendIcon from "../../Resource/Icon/expend-icon.svg";
 import shrinkIcon from "../../Resource/Icon/shrink-icon.svg";
+import EditIcon from "../../Resource/Icon/edit.svg";
 
 import classNames from "classnames";
 export function LineCard() {
@@ -34,17 +35,19 @@ export function LineCard() {
   const firstStation = stations[0];
   const lastStation = stations[stations.length - 1];
   const [expend, setExpend] = useState(false);
+  const [edit, setEdit] = useState(false);
   const [expendWidth, setExpendWidth] = useState(455);
+  const [tab, setTab] = useState("name");
   const getExpendWidth = () => {
     const expected = 33 + 161 * stations.length;
     const width =
       expected > window.innerWidth ? window.innerWidth - 100 : expected;
-      console.log(width)
+    console.log(width);
     setExpendWidth(width);
   };
   useEffect(() => {
     window.addEventListener("resize", getExpendWidth);
-    return ()=>window.removeEventListener("resize", getExpendWidth);
+    return () => window.removeEventListener("resize", getExpendWidth);
   }, [stations]);
   useEffect(() => {
     getExpendWidth();
@@ -55,20 +58,30 @@ export function LineCard() {
       style={expend ? { width: expendWidth } : {}}
     >
       <div className="tools">
-        <div className="edit"></div>
-        <div className="expend" onClick={() => setExpend(!expend)}>
-          <img
-            className="expend-icon"
-            src={expend ? shrinkIcon : expendIcon}
-          ></img>
-        </div>
+        {expend || edit ? (
+          <></>
+        ) : (
+          <div className="edit tool-item" onClick={() => setEdit(true)}>
+            <EditIcon className="edit-icon"></EditIcon>
+          </div>
+        )}
+        {edit ? (
+          <></>
+        ) : (
+          <div className="expend tool-item" onClick={() => setExpend(!expend)}>
+            <img
+              className="expend-icon"
+              src={expend ? shrinkIcon : expendIcon}
+            ></img>
+          </div>
+        )}
       </div>
       <div className="line-name">{lineName}</div>
       <div className="stations-count">{stations.length}个站点</div>
       <div className="from-to">
         从{firstStation.stationName}开往{lastStation.stationName}
       </div>
-      <div className="station-bar" style={expend ? { width: expendWidth} : {}}>
+      <div className="station-bar" style={expend ? { width: expendWidth } : {}}>
         <div className="add-first"></div>
         {stations.map((station) => {
           const { stationName, bendFirst } = station;
@@ -93,6 +106,53 @@ export function LineCard() {
           );
         })}
       </div>
+      {
+        <div className={classNames({ "edit-panel": 1, edit })}>
+          <div
+            className={classNames({
+              "edit-tools": 1,
+            })}
+          >
+            <div
+              className={classNames({
+                "edit-tool name": 1,
+                selected: tab === "name",
+              })}
+              onClick={()=>setTab("name")}
+            >
+              <div className="title">名称</div>
+              <div className="value">1号线</div>
+            </div>
+            <div
+              className={classNames({
+                "edit-tool color": 1,
+                selected: tab === "color",
+              })}
+              onClick={()=>setTab("color")}
+
+            >
+              <div className="title">颜色</div>
+              <div className="value">品红</div>
+            </div>
+            <div
+              className={classNames({
+                "edit-tool operation": 1,
+                selected: tab === "operation",
+              })}
+              onClick={()=>setTab("operation")}
+
+            >
+              <div className="title">操作</div>
+              <div className="value">删除</div>
+            </div>
+          </div>
+
+          <div className="done" onClick={() => setEdit(false)}>
+            <EditIcon className="done-icon" />
+            <span className="done-des">完成</span>
+          </div>
+        </div>
+      }
     </div>
   );
 }
