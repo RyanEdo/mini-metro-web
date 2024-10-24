ab2b0e6b83ad85c390082d79b05ba51ede61fec7
ryan <ryanedo@qq.com>
2024-10-24 19:35:39 +0800

    export

--- src/WelcomeTour/Steps/export.ts
+++ src/WelcomeTour/Steps/export.ts
@@ -1,6 +1,6 @@
 import { DriveStep, Driver } from "driver.js";
 import { browserInfo } from "../../Common/util";
-
+import {t} from 'i18next';
 export const getSteps = (driver: Driver): DriveStep[] => {
   const { engine } = browserInfo;
   const webkit = engine.name === "WebKit";
@@ -8,23 +8,23 @@ export const getSteps = (driver: Driver): DriveStep[] => {
   {
     element: ".title .click-panel",
     popover: {
-      title: "点击标题",
-      description: "点击标题打开菜单",
+      title: t('dian-ji-biao-ti'),
+      description: t('dian-ji-biao-ti-da-kai-cai-dan'),
     },
   },
   {
     element: ".existed-map-btn",
     popover: {
-      title: "从模板新建",
-      description: "在已有的地图上修改",
+      title: t('cong-mo-ban-xin-jian'),
+      description: t('zai-yi-you-de-di-tu-shang-xiu-gai'),
     },
   },
   {
     element: ".tools",
     onHighlighted:()=>{},
     popover: {
-      title: "选择一个你喜欢的城市",
-      description: "然后点击下一步",
+      title: t('xuan-ze-yi-ge-ni-xi-huan-de-cheng-shi'),
+      description: t('ran-hou-dian-ji-xia-yi-bu'),
       showButtons:["next"],
       onNextClick:()=>{
         driver.moveNext();
@@ -34,30 +34,30 @@ export const getSteps = (driver: Driver): DriveStep[] => {
   {
     element: ".confirm-add-from-existed-map-btn",
     popover: {
-      title: "以此为模板新建",
-      description: "确认新建前请确保已经保存了当前的地图",
+      title: t('yi-ci-wei-mo-ban-xin-jian'),
+      description: t('que-ren-xin-jian-qian-qing-que-bao-yi-jing-bao-cun-le-dang-qian-de-di-tu'),
     },
   },
   {
     element: ".title .click-panel",
     popover: {
-      title: "打开菜单",
-      description: "选择作为文件导出",
+      title: t('da-kai-cai-dan'),
+      description: t('xuan-ze-zuo-wei-wen-jian-dao-chu'),
     },
   },
   {
     element: ".export-as-file-btn",
     popover: {
-      title: "作为文件导出",
-      description: "选择作为文件导出",
+      title: t('zuo-wei-wen-jian-dao-chu'),
+      description: t('xuan-ze-zuo-wei-wen-jian-dao-chu-0'),
     },
   },
   {
     element: ".import-file-btn",
     onHighlighted:()=>{},
     popover: {
-      title: "导入刚才导出文件",
-      description: "下次点击导入文件可以继续编辑",
+      title: t('dao-ru-gang-cai-dao-chu-wen-jian'),
+      description: t('xia-ci-dian-ji-dao-ru-wen-jian-ke-yi-ji-xu-bian-ji'),
       showButtons:["next"],
       onNextClick:driver.moveNext
     },
@@ -65,22 +65,22 @@ export const getSteps = (driver: Driver): DriveStep[] => {
   {
     element: `.export-as${webkit?'-svg':''}-image-btn`,
     popover: {
-      title: "导出图片",
-      description: "点击导出图片。导出可能会导致卡顿几秒属于正常现象",
+      title: t('dao-chu-tu-pian'),
+      description: t('dian-ji-dao-chu-tu-pian-dao-chu-ke-neng-hui-dao-zhi-ka-dun-ji-miao-shu-yu-zheng-chang-xian-xiang'),
     },
   },
   {
     element: `.recover-btn`,
     popover: {
-      title: "从缓存中恢复数据",
-      description: "如果没保存的时候不小心刷新，可以点击这里恢复数据",
+      title: t('cong-huan-cun-zhong-hui-fu-shu-ju'),
+      description: t('ru-guo-mei-bao-cun-de-shi-hou-bu-xiao-xin-shua-xin-ke-yi-dian-ji-zhe-li-hui-fu-shu-ju'),
     },
   },
   {
     element: `.export-recover-btn`,
     popover: {
-      title: "导出恢复数据",
-      description: "如果某一步操作后导致报错或崩溃，点击这里导出最后一次正确的数据。（如果遇到这种情况，请反馈给作者）",
+      title: t('dao-chu-hui-fu-shu-ju'),
+      description: t('ru-guo-mou-yi-bu-cao-zuo-hou-dao-zhi-bao-cuo-huo-beng-kui-dian-ji-zhe-li-dao-chu-zui-hou-yi-ci-zheng-que-de-shu-ju-ru-guo-yu-dao-zhe-zhong-qing-kuang-qing-fan-kui-gei-zuo-zhe'),
     },
   },
 ];
