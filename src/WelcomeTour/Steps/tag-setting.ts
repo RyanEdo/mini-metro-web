764568bfbda17614947aca2b719bea16d221ef52
ryan <ryanedo@qq.com>
2024-10-24 19:43:30 +0800

    tag

--- src/WelcomeTour/Steps/tag-setting.ts
+++ src/WelcomeTour/Steps/tag-setting.ts
@@ -1,48 +1,48 @@
 import { DriveStep, Driver } from "driver.js";
-
+import {t} from 'i18next';
 export const getSteps = (driver: Driver): DriveStep[] => [
   {
     element: ".title .click-panel",
     popover: {
-      title: "点击标题",
-      description: "点击标题打开菜单",
+      title: t('dian-ji-biao-ti'),
+      description: t('dian-ji-biao-ti-da-kai-cai-dan-1'),
     },
   },
   {
     element: ".auto-hidden-btn",
     popover: {
-      title: "关闭自动隐藏",
-      description: "站点名会在地图缩放到小尺度时自动隐藏。关闭自动隐藏以确保可以一直显示站点名称",
+      title: t('guan-bi-zi-dong-yin-cang'),
+      description: t('zhan-dian-ming-hui-zai-di-tu-suo-fang-dao-xiao-chi-du-shi-zi-dong-yin-cang-guan-bi-zi-dong-yin-cang-yi-que-bao-ke-yi-yi-zhi-xian-shi-zhan-dian-ming-cheng'),
     },
   },
   {
     element: ".menu",
     popover: {
-      title: "点击任意空白区域退出菜单",
-      description: "退出菜单",
+      title: t('dian-ji-ren-yi-kong-bai-qu-yu-tui-chu-cai-dan-0'),
+      description: t('tui-chu-cai-dan-0'),
     },
   },
   {
     element: ".station-descend-31",
     popover: {
-      title: "点击站点",
-      description: "以打开站点卡片",
+      title: t('dian-ji-zhan-dian'),
+      description: t('yi-da-kai-zhan-dian-ka-pian-1'),
     },
   },
   {
     element: ".edit-tools",
-    popover: { title: "在这里向下滚动一下", description: "再点击标签按钮" },
+    popover: { title: t('zai-zhe-li-xiang-xia-gun-dong-yi-xia'), description: t('zai-dian-ji-biao-qian-an-niu') },
   },
   {
     element: ".tag-detail",
-    popover: { title: "尝试改变站点名到右下角", description: "点击右下角的灰色方块" },
+    popover: { title: t('chang-shi-gai-bian-zhan-dian-ming-dao-you-xia-jiao'), description: t('dian-ji-you-xia-jiao-de-hui-se-fang-kuai') },
   },
   {
     element: ".station-name-descend-31",
-    popover: { title: "可以看到站点名已经位于站点右下角了", description: "可以用这种方法修改站点名位置", showButtons:["next"],onNextClick:driver.moveNext },
+    popover: { title: t('ke-yi-kan-dao-zhan-dian-ming-yi-jing-wei-yu-zhan-dian-you-xia-jiao-le'), description: t('ke-yi-yong-zhe-zhong-fang-fa-xiu-gai-zhan-dian-ming-wei-zhi'), showButtons:["next"],onNextClick:driver.moveNext },
   },
   {
     element: ".tag-item.center",
-    popover: { title: "点击中间的文字恢复到自动选择位置", description: "恢复到自动选择位置" },
+    popover: { title: t('dian-ji-zhong-jian-de-wen-zi-hui-fu-dao-zi-dong-xuan-ze-wei-zhi'), description: t('hui-fu-dao-zi-dong-xuan-ze-wei-zhi') },
   },
 ];
